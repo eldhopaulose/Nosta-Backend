ebfc8b6e873cf12b7bfb965658de0136bc6b6cba
eldhopaulose <eldhopaulose0485@gmail.com>
2024-04-11 00:55:20 -0700

    user otp to convert to 5 digit

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ async function sendOTP(email, otp) {
 
 // Generate OTP
 function generateOTP() {
-  return Math.floor(100000 + Math.random() * 900000).toString();
+  return Math.floor(10000 + Math.random() * 90000).toString();
 }
 
 userSchema.statics.sendOTP = async function (email, name) {
