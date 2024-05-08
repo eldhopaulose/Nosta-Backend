af38a3053353ef31daae4eb40cfee871e4828dd3
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-05-08 17:17:38 +0530

    oder test 3

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,21 +1,34 @@
 const Order = require("../models/order");
+const Cart = require("../models/cart");
 
 exports.orderPlaced = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    // If the request contains productId and totalCost, it implies an order is being placed directly
     const { productId, totalCost, address } = req.body;
 
-    if (productId && totalCost && address) {
-      const newOrder = new Order({
-        userId,
-        items: [{ productId, totalCost }],
-        address,
-      });
-      const savedOrder = await newOrder.save();
-      return res.status(201).json(savedOrder);
+    if (!productId || !totalCost || !address) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    const userCart = await Cart.findOne({ userId }).populate("items.productId");
+
+    if (!userCart) {
+      throw new Error("User's cart not found");
+    }
+
+    // Map cart items to order items
+    const orderItems = userCart.items.map((item) => ({
+      productId: item.productId._id,
+      quantity: item.quantity,
+      totalCost: item.totalCost,
+    }));
+
+    const newOrder = new Order({
+      items: orderItems,
+    });
+    const savedOrder = await newOrder.save();
+    res.status(201).json(savedOrder);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
