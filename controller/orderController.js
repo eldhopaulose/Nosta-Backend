6f6ce892aa6b110589b15c9240363c3f79c65091
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-05-08 17:09:08 +0530

    oder test 2

--- controller/orderController.js
+++ controller/orderController.js
@@ -4,17 +4,18 @@ exports.orderPlaced = async (req, res) => {
   try {
     const userId = req.user._id;
 
+    // If the request contains productId and totalCost, it implies an order is being placed directly
     const { productId, totalCost, address } = req.body;
 
-    if (!productId || !totalCost || !address) {
-      return res.status(400).json({ message: "All fields are required" });
+    if (productId && totalCost && address) {
+      const newOrder = new Order({
+        userId,
+        items: [{ productId, totalCost }],
+        address,
+      });
+      const savedOrder = await newOrder.save();
+      return res.status(201).json(savedOrder);
     }
-
-    const newOrder = new Order({
-      items: [{ productId, totalCost, address, userId }],
-    });
-    const savedOrder = await newOrder.save();
-    res.status(201).json(savedOrder);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- models/order.js
+++ models/order.js
@@ -6,53 +6,53 @@ const orderSchema = new mongoose.Schema({
     type: Number,
     unique: true,
   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   items: [
     {
-      userId: {
+      productId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
         required: true,
       },
-      productId: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Cart.productId",
-          required: true,
-        },
-      ],
-
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
       totalCost: {
         type: Number,
         required: true,
       },
-      address: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Address",
-        required: true,
-      },
-      status: {
-        type: String,
-        default: "Paid",
-      },
-      billDate: {
-        type: Date,
-        default: () => {
-          const now = new Date();
-          return now;
-        },
-      },
-      deliveryDate: {
-        type: Date,
-        default: () => {
-          const now = new Date();
-          now.setDate(now.getDate() + 7);
-          return now;
-        },
-      },
     },
   ],
+  address: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Address",
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Paid",
+  },
+  billDate: {
+    type: Date,
+    default: () => {
+      const now = new Date();
+      return now;
+    },
+  },
+  deliveryDate: {
+    type: Date,
+    default: () => {
+      const now = new Date();
+      now.setDate(now.getDate() + 7);
+      return now;
+    },
+  },
 });
-
 // Plugin to auto-increment orderId
 orderSchema.plugin(AutoIncrement, { inc_field: "orderId" });
 
