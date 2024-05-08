469a90491af1f946daebb8cdc62ed38c73a6014b
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-05-08 16:51:17 +0530

    oder_test 1

--- models/order.js
+++ models/order.js
@@ -16,7 +16,7 @@ const orderSchema = new mongoose.Schema({
       productId: [
         {
           type: mongoose.Schema.Types.ObjectId,
-          ref: "Cart",
+          ref: "Cart.productId",
           required: true,
         },
       ],
