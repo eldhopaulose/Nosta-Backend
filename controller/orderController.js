3e383548ed82f16d7c01ce788d6ab9d8f136ef7b
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-05-09 17:06:53 +0530

    oder test 7

--- controller/orderController.js
+++ controller/orderController.js
@@ -7,20 +7,28 @@ exports.orderPlaced = async (req, res) => {
     console.log(userId);
     const { totalCost, address } = req.body;
 
-    if (!addressId) {
-      return res.status(400).json({ message: "Address ID is required" });
+    // Assuming address is the correct variable to check
+    if (!address) {
+      return res.status(400).json({ message: "Address is required" });
     }
 
     const cart = await Cart.findOne({ userId });
     if (!cart) {
       throw new Error("No cart found for this user.");
     }
+
     // Map cart items to order items
+    // Assuming you have a function to map and calculate totalCost for each item
+    const orderItems = cart.items.map((item) => ({
+      productId: item.productId,
+      quantity: item.quantity,
+      // Calculate the total cost for each item
+    }));
 
     // Create the order
     const newOrder = new Order({
       userId,
-      items: cart.items,
+      items: orderItems,
       address: address,
       totalCost,
     });
@@ -29,14 +37,15 @@ exports.orderPlaced = async (req, res) => {
     const savedOrder = await newOrder.save();
 
     // Clear the user's cart after placing the order
-    userCart.items = [];
-    await userCart.save();
+    cart.items = [];
+    await cart.save();
 
     res.status(201).json(savedOrder);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 exports.getAllOrders = async (req, res) => {
   try {
     const orders = await Order.find()
